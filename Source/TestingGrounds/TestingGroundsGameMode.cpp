484f9818d69254e6844bdaf60f7c8445511ed9e0
Ricardo Dias <ricardodc@gmail.com>
2017-02-06 04:03:14 -0200

    No Gun

--- Source/TestingGrounds/TestingGroundsGameMode.cpp
+++ Source/TestingGrounds/TestingGroundsGameMode.cpp
@@ -10,7 +10,7 @@ ATestingGroundsGameMode::ATestingGroundsGameMode()
 {
 	// set default pawn class to our Blueprinted character
 	
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behaviour/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Behaviour/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
