a488cca4dc49f405c2a7fb28e19dc84f5cf2529a
Ricardo Dias <ricardodc@gmail.com>
2017-02-18 23:28:50 -0300

    Major refactory and implementation of new behaviour tree

--- Source/TestingGrounds/TestingGroundsGameMode.cpp
+++ Source/TestingGrounds/TestingGroundsGameMode.cpp
@@ -10,7 +10,7 @@ ATestingGroundsGameMode::ATestingGroundsGameMode()
 {
 	// set default pawn class to our Blueprinted character
 	
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Behaviour/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Character/Behaviour/Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
